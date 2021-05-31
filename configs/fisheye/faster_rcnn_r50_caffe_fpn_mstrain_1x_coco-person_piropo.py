e785d2309ed3faf03d27c0fb79f822d96301f846
Thaddäus Wiedemer <thaddaeus.wiedemer@gmail.com>
2021-06-01 02:45:40 +0800

    updated config

--- configs/fisheye/faster_rcnn_r50_caffe_fpn_mstrain_1x_coco-person_piropo.py
+++ configs/fisheye/faster_rcnn_r50_caffe_fpn_mstrain_1x_coco-person_piropo.py
@@ -7,8 +7,8 @@ img_norm_cfg = dict(mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rg
 
 data = dict(
     test=dict(
-        ann_file=data_root + 'omni_all.json',
+        ann_file=data_root + 'omni_tests.json',
         img_prefix=data_root))
 
-load_from = '../../checkpoints/faster_rcnn_r50_fpn_1x_coco-person_20201216_175929-d022e227.pth'
+load_from = '../../checkpoints/ '
 work_dir = 'work_dirs/blub'
