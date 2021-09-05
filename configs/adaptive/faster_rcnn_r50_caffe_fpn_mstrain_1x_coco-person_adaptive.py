598264f855e29fdedb60fdbd203b680035bd2f8b
Thaddäus Wiedemer <thaddaeus.wiedemer@gmail.com>
2021-09-06 07:07:26 +0800

    validate new framework

--- configs/adaptive/faster_rcnn_r50_caffe_fpn_mstrain_1x_coco-person_adaptive.py
+++ configs/adaptive/faster_rcnn_r50_caffe_fpn_mstrain_1x_coco-person_adaptive.py
@@ -107,7 +107,7 @@ img_norm_cfg = dict(mean=[103.53, 116.28, 123.675], std=[1.0, 1.0, 1.0], to_rgb=
 train_pipeline_src = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
+    dict(type='Resize', img_scale=(800, 800), keep_ratio=False),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -118,7 +118,7 @@ train_pipeline_src = [
 train_pipeline_tgt = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
+    dict(type='Resize', img_scale=(800, 800), keep_ratio=False),
     dict(type='RandomFlip', flip_ratio=0.5),
     # dict(type='Albu',
     #      transforms=[dict(type='RandomRotate90', p=1.0)],
